be4ab541109236bcce84cd1abe644ca81e203507
Chaston Harper <chastonharper1@gmail.com>
2020-05-27 10:20:39 -0500

    added paragraph with text that changes to a random color when clicked

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
 document.addEventListener("DOMContentLoaded", function (){
 
-var btn = document.createElement("BUTTON");
-btn.innerHTML = "DOM";
-btn.addEventListener("click", function(){alert("Hello World!");
-});
-document.body.appendChild(btn);
+    var btn = document.createElement("BUTTON");
+    btn.innerHTML = "DOM";
+    btn.addEventListener("click", function(){alert("Hello World!");
+    });
+    document.body.appendChild(btn);
 
-document.getElementById("#myBtn");
+    document.getElementById("#myBtn");
 
-let myBtn = document.getElementById("myBtn");
-myBtn.addEventListener("click", click);
-function click() {
-    let message = document.getElementById("someText").value;
-    alert(message);
-}
+    let myBtn = document.getElementById("myBtn");
+    myBtn.addEventListener("click", click);
+    function click() {
+        let message = document.getElementById("someText").value;
+        alert(message);
+    }
+
+    var para = document.createElement("p");
+    var node = document.createTextNode("Paragraph text.");
+    para.appendChild(node);
+    var element = document.getElementById("p");
+    element.appendChild(para);
+    para.onclick = changeColor;
+    function changeColor() {
+        document.body.style.color = "purple";
+        return false;
+    }
+
+    let p = document.getElementById("p");
+    p.addEventListener("click", function() {
+        let allchar = "0123456789ABCDEF";
+        let randcol = "";
+        for (let i = 0; i < 6; i++) {
+            randcol += allchar[Math.floor(Math.random() * 16)];
+        }
+        p.style.color = "#" + randcol;
+   
+})
 
 })
 
